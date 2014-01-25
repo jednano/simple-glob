10cbe7da2b826203470df39c362da30a6df2e982
Jed Hunsaker <jed.hunsaker@gmail.com>
2014-01-25 10:04:22 -0600

    Add initial code

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+﻿/*
+ * grunt
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2013 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    nodeunit: {
+      all: ['test/*.js']
+    },
+    jshint: {
+      gruntfile_tasks: ['Gruntfile.js'],
+      libs_n_tests: ['lib/**/*.js', '<%= nodeunit.all %>'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: 'nofunc',
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+      }
+    },
+    watch: {
+      libs_n_tests: {
+        files: ['<%= jshint.libs_n_tests %>'],
+        tasks: ['jshint:libs_n_tests', 'nodeunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // "npm test" runs these tasks
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
+
+  // Default task.
+  grunt.registerTask('default', ['test']);
+
+};
